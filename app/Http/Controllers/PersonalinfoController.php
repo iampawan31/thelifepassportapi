d0e589727bdd13aa83379a54c75048aa92bc554c
Md Prawez Musharraf <mdprawez.musharraf@gmail.com>
2020-01-26 17:14:43 +0530

    commit changes

--- app/Http/Controllers/PersonalinfoController.php
+++ app/Http/Controllers/PersonalinfoController.php
@@ -217,7 +217,7 @@ class PersonalinfoController extends Controller
     public function update(Request $request, PersonalInfo $personalInfo)
     {
         $inputs = $request->all();
-        
+        dd($inputs);
         //personal information
         $legal_name         = $inputs['legal_name'];
         $nick_name          = $inputs['nickname'];
--- public/js/app.js
+++ public/js/app.js
@@ -109604,8 +109604,8 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/pawankumar/valetcode/thelifepassportapi/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/pawankumar/valetcode/thelifepassportapi/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/mdprawezmusharraf/Sites/thelifepassportapi/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/mdprawezmusharraf/Sites/thelifepassportapi/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
